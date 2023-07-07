6d6db3aba32abe49740fdac95a6da08144598510
Ross Kulyk <uaross@hotmail.com>
2023-07-07 14:21:11 -0400

    adding firebase package, and adding sign-in page to the router

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import "./App.css";
 import { Routes, Route, Outlet } from "react-router-dom";
 import Home from "./routes/home/home.component";
 import NavigationBar from "./routes/navigation/navigation.component";
+import SignIn from "./routes/sign-in/signin.component";
 
+const Shop = () => {
+  return <h1>Shop page</h1>;
+};
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<NavigationBar />}>
         <Route path="/" element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+        <Route path="signin" element={<SignIn />} />
       </Route>
     </Routes>
   );
