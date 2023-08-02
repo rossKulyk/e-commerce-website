89fcce9605131777250837879ceaa2b6adaf2085
Ross Kulyk <uaross@hotmail.com>
2023-08-02 10:37:10 -0400

    setting the redux store

--- src/store/store.js
+++ src/store/store.js
@@ -1,2 +1,9 @@
 import { compose, createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
+import { rootReducer } from "./root-reducer";
+
+const middleware = [logger];
+// applyMiddleware intercepts actions before they reach the reducers, allows to perform tasks like
+// logging, handling asynchronous actions, or modifying actions before they update the state
+const composeEnhancer = compose(applyMiddleware(...middleware));
+export const store = createStore(rootReducer, undefined, composeEnhancer);
