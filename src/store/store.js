4133dbe1b0ee42f24ffcfd0b1a90fed7521ec384
Ross Kulyk <uaross@hotmail.com>
2023-08-07 09:20:05 -0400

    implementing redux-devtools into the code

--- src/store/store.js
+++ src/store/store.js
@@ -13,11 +13,23 @@ const persistConfig = {
 
 // create persist reducer using persist config
 const persistedReducer = persistReducer(persistConfig, rootReducer);
-const middleware = [logger];
+// should be run only in dev, not production code
+const middleware = [process.env.NODE_ENV !== "production" && logger].filter(
+  Boolean // filters everything that's not true
+);
+
+// if not in production env and window obj && dev tools exist, otherwise use compose of redux
+const composeEnhancer =
+  (process.env.NODE_ENV !== "production" &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
 
 // applyMiddleware intercepts actions before they reach the reducers, allows to perform tasks like
 // logging, handling asynchronous actions, or modifying actions before they update the state
-const composeEnhancer = compose(applyMiddleware(...middleware));
-export const store = createStore(persistedReducer, undefined, composeEnhancer);
-// 
+const composeEnhancers = composeEnhancer(applyMiddleware(...middleware));
+// const composeEnhancers = compose(applyMiddleware(...middleware));
+
+export const store = createStore(persistedReducer, undefined, composeEnhancers);
+//
 export const persistor = persistStore(store);
